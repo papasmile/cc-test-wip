70ad6801ed1fc0b203c3e59d924ead340e4f0b88
Papa <ismail@azizahsolutions.biz>
2022-02-12 17:24:30 -0500

    wip

--- framework/base.js
+++ framework/base.js
@@ -1,30 +1,57 @@
 import request from 'supertest';
 import { expect } from 'chai';
+import mysql from 'mysql';
 
 const req = request('https://getcampground-ntxpstvepa-uc.a.run.app')
 
-const testo = (args, extraInit) => {
-  const updates = {}; // table: row count
+var connection = mysql.createConnection({
+  host     : '34.122.228.45',
+  user     : 'cceng',
+  password : 'Eng56THabM7u',
+  database : 'Campgrounds'
+});
 
-  before(() => {
-    console.log(args);
-    // for each arg key, delete table, populate data from table
-    extraInit && extraInit();
-  })
+connection.connect();
 
-  describe("Setup", () => {
-    it("does work", () => {
-      // read db and validate updates
-      expect(true).to.equal(true);
+const setupTest = (dataFile, extraInit) => {
+
+  before(done => {
+    fs.readFile(dataFile,'utf-8', (err,jsonString) => {
+        const testData = JSON.parse(jsonString);
+        doStuff(testData, () => {
+          extraInit(testData);
+          done();
+        });
     });
   });
 
 };
 
-export default testo;
+const doStuff = (testData, complete) => {
+  const updates = {}; // table: row count
+  const ticker = 0;
+
+  // for each arg key, delete table, populate data from table
+  Object.keys(testData).forEach(tableName => {
+    testData[tableName].forEach(item => {
+    ticker++;
+    connection.query(`DELETE FROM ${tableName}`, (error, results, fields) => {
+        connection.query(`INSERT INTO ${tableName} SET ?`, item, (error, results, fields) => {
+          console.log(`row inserted into ${tableName}`);
+          console.log(error);
+          console.log(results);
+          ticker--;
+          if (ticker == 0) complete();
+        });
+      });
+    })
+  });
+};
+
+export default setupTest;
 
 export const testSuccess = (scenario, url, handleSuccess) => {
-  it(scenario, function(done) {
+  it(scenario, done => {
     this.timeout(20000);
     req.get('/eng' + url)
       .set('X-API-Auth', 'daa49316-bc03-4811-9781-d74c0defa62e')
--- test/getCampgroundTest.js
+++ test/getCampgroundTest.js
@@ -4,13 +4,11 @@ import dataSetup, { testSuccess } from '../framework/base.js';
 import fs from 'fs';
 
 
-describe("getCampground", async () => {
-  let testData;
+let testData;
 
-  await fs.readFile('./test/getCampground.json','utf-8', (err,jsonString) => {
-      testData = JSON.parse(jsonString);
-      dataSetup(testData);
-   });
+dataSetup('./test/getCampground.json', data => testData = data);
+
+describe("getCampground", async () => {
 
   describe('Primary Flow', () => {
     testSuccess('should return data properly', '/getCampground', response => {
