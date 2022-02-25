f3be704cf7ba50507857db0943b5b1a87be5ce15
Papa <ismail@azizahsolutions.biz>
2022-02-24 21:51:31 -0500

    wip:

--- test/getAvailableTest.js
+++ test/getAvailableTest.js
@@ -4,6 +4,45 @@ import dataSetup, { testSuccess } from '../framework/base.js';
 
 let testData;
 
-// dataSetup('./test/getAvailable.json', data => testData = data);
+dataSetup('./test/getAvailable.json', data => testData = data);
 
-// curl -H "X-API-Auth: daa49316-bc03-4811-9781-d74c0defa62e" "https://getcampground-ntxpstvepa-uc.a.run.app/eng/getAvailable?Checkin=2020-01-01T00:00:00.000Z&Checkout=2030-01-01T00:00:00.000Z"
+describe('getAvailable', () => {
+
+  describe('Primary Flow', () => {
+
+    // single day stay
+    const simpleUrl = '/getAvailable?Checkin=2032-01-02T00:00:00.000Z&Checkout=2032-01-03T00:00:00.000Z';
+
+    testSuccess('should return data properly', simpleUrl, response => {
+      const subResponse = response.Campsites;
+
+      if (!(subResponse && subResponse.length)) assert.fail('No campsites found in response');
+
+      const campsiteResponse = subResponse[0];
+      const campsiteData = testData.Campsites[0];
+
+      expect(campsiteResponse.available).to.equal(true);
+      expect(campsiteResponse.name).to.equal(campsiteData.name);
+      expect(campsiteResponse.siteType).to.equal(campsiteData.siteType);
+      expect(campsiteResponse.slabType).to.equal(campsiteData.slabType);
+      expect(campsiteResponse.length).to.equal(campsiteData.length);
+      expect(campsiteResponse.width).to.equal(campsiteData.width);
+      expect(campsiteResponse.maxTrailerSize).to.equal(campsiteData.maxTrailerSize);
+      expect(campsiteResponse.minTrailerSize).to.equal(campsiteData.minTrailerSize);
+      expect(campsiteResponse.electric).to.equal(campsiteData.electric);
+      expect(campsiteResponse.water).to.equal(campsiteData.water);
+      expect(campsiteResponse.sewer).to.equal(campsiteData.sewer);
+      expect(campsiteResponse.cable).to.equal(campsiteData.cable);
+      expect(campsiteResponse.wifi).to.equal(campsiteData.wifi);
+      expect(campsiteResponse.firepit).to.equal(campsiteData.firepit);
+      expect(campsiteResponse.picnicTable).to.equal(campsiteData.picnicTable);
+      expect(campsiteResponse.grill).to.equal(campsiteData.grill);
+      expect(campsiteResponse.minDays).to.equal(campsiteData.minDays);
+      expect(campsiteResponse.blockDays).to.equal(campsiteData.blockDays);
+      expect(campsiteResponse.rvSite).to.equal(campsiteData.RVSite);
+      expect(campsiteResponse.monthlySite).to.equal(campsiteData.monthlySite);
+    });
+
+  });
+
+});
--- test/getReservationsTest.js
+++ test/getReservationsTest.js
@@ -14,8 +14,6 @@ describe('getReservations', () => {
     const simpleUrl = '/getReservations?Checkin=2032-01-02T00:00:00.000Z&Checkout=2032-01-03T00:00:00.000Z';
 
     testSuccess('should return data properly', simpleUrl, response => {
-      console.log(Object.keys(response));
-
       const subResponse = response.Reservations;
 
       if (!subResponse) fail('No reservation found in response');
@@ -33,12 +31,15 @@ describe('getReservations', () => {
       expect(reservationResponse.ManagerUpdated).to.equal(reservationData.managerUpdated);
       expect(reservationResponse.Checkin).to.equal('2032-01-02T00:00:00Z');
       expect(reservationResponse.Checkout).to.equal('2032-01-03T00:00:00Z');
-      expect(reservationResponse.TotalCharge).to.equal(reservationData.managementReserved);
-      expect(reservationResponse.ManagementReserved).to.equal(reservationData.managementReserved);
-      expect(reservationResponse.ManagementReserved).to.equal(reservationData.managementReserved);
-      expect(reservationResponse.ManagementReserved).to.equal(reservationData.managementReserved);
-
-
+      expect(reservationResponse.TotalCharge).to.equal(reservationData.totalCharge);
+      expect(reservationResponse.BaseRate).to.equal(reservationData.baseRate);
+      expect(reservationResponse.Taxes).to.equal(reservationData.taxes);
+      expect(reservationResponse.ProcessingFee).to.equal(reservationData.processingFee);
+      expect(reservationResponse.DiscountAmount).to.equal(reservationData.discountAmount);
+      expect(reservationResponse.DynamicAdjust).to.equal(reservationData.dynamicAdjust);
+      expect(reservationResponse.DiscountId).to.equal(reservationData.discountId);
+      expect(reservationResponse.CustomerNotes).to.equal(reservationData.customerNotes);
+      expect(reservationResponse.Cancelled).to.equal('0001-01-01T00:00:00Z');
     });
 
   });
